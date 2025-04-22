ffccf4b7202625b20b9cc4f6e80fba0e90167fbf
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-04-22 16:58:44 -0400

    Added NavBar component and added basic code

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/sightings">Sightings</NavLink>
+            <NavLink to="/species">Species</NavLink>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Signup</NavLink>
+        </nav>
+    )
+}
+
+export default NavBar;
