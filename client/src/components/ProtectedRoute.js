e3894b2f13286659340d2ba86fca13f04574a9af
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-04-30 13:36:00 -0400

    created ProtectedRoute component and added in basic code to check if a user is authenticated and redirect to the login page if not

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,17 @@
+// This component is used to protect routes that require authentication
+// It checks if the user is authenticated and redirects to the login page if not
+
+import { Navigate, useOutletContext } from "react-router-dom";
+
+function ProtectedRoute({children}) {
+    const { user } = useOutletContext();
+    if (!user) {
+        return <Navigate to="/login" />;
+    }
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
