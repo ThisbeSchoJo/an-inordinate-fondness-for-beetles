ab9784668c2e8336278fa34c6de8a0c2057b0e50
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-05-04 17:27:57 -0400

    Added an Add Friend button on profile, removed the Log a Sighting button from profile, imported useState into Profile and added new state varaibles to store the search input and search results

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../home.css";
 
 function Home() {
+
+    const fireflyRef = useRef(null);
+    const angleRef = useRef(0);
+
+  useEffect(() => {
+    const firefly = fireflyRef.current;
+
+    if (!firefly) return;
+
+    firefly.style.position = "absolute";
+    firefly.style.transition = "none"
+
+    let angle = 0;
+    const speed = 0.01;
+    const radius = 100;
+    let baseX = window.innerWidth / 2;
+    let baseY = window.innerHeight / 2;
+
+    function animate() {
+        angle += speed;
+        const offsetX = Math.cos(angle) * radius;
+        const offsetY = Math.sin(angle * 1.5) * radius;
+
+        firefly.style.left = `${baseX + offsetX}px`;
+        firefly.style.top = `${baseY + offsetY}px`;
+        
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    return () => cancelAnimationFrame(animate);
+  }, []);
+
+//     function moveFirefly() {
+//       const maxX = window.innerWidth - firefly.width;
+//       const maxY = window.innerHeight - firefly.height;
+
+//       const randomX = Math.random() * maxX;
+//       const randomY = Math.random() * maxY;
+
+//       firefly.style.left = `${randomX}px`;
+//       firefly.style.top = `${randomY}px`;
+//     }
+
+//     const interval = setInterval(moveFirefly, 1000); // move every 3s slowly
+
+//     return () => clearInterval(interval); // cleanup on unmount
+//   }, []);
+
     return (
         <>
             <h1>Welcome to Firefly Finder</h1>
             <p>Find and share firefly sightings near you.</p>
-            <div className="firefly-logo-container">
-                <img src="/firefly-logo.gif" alt="Firefly Logo" />
-            </div>
+            <img id="firefly" ref={fireflyRef} src="/firefly-logo.gif" alt="Firefly Logo" style={{ width: "50px", height: "50px", pointerEvents: "none" }}/>
         </>
     )
 }
